aba397b0908d46988fee77d987d4a2c186330196
Wojak14 <andrzejbrodziuk@wp.pl>
2026-01-17 14:02:07 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
-const CACHE_NAME = "pasieka-2026-v1";
-const FILES = ["./", "index.html"];
+const CACHE_NAME = 'kalendarz2026-cache-v1';
+const urlsToCache = ['index.html','manifest.json','bee_icon_192x192.png','bee_icon_512x512.png'];
 
-self.addEventListener("install", e => {
+self.addEventListener('install', e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(response=>{
+      return response || fetch(e.request);
+    })
   );
 });
